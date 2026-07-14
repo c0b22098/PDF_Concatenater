b9be45c672a7050608f188dc51a1e28128e07fbf
Njma <g212602618@edu.teu.ac.jp>
2026-07-14 19:34:48 +0900

    1~6回の提出物に対応する正規表現のリストを作成

--- regex_config.js
+++ regex_config.js
@@ -1,15 +1,36 @@
 // 提出物のURLのリストを7回分用意して、切り替えする用
 const REGEX_LISTS = {
+    1: [
+        "https://github\.com/.*/commits/main",
+        "https://github\.com/.*/blob/main/flying_kokaton\.py"
+    ],
+    2: [
+        "https://github\.com/.*/commits/main",
+        "https://github\.com/.*/blob/main/dodge_bomb\.py",
+        "https://github\.com/.*/issues"
+    ],
+    3: [
+        "https://github\.com/.*/blob/main/fight_kokaton\.py",
+        "https://github\.com/.*/branches",
+        "https://github\.com/.*/compare/.*"
+    ],
+    4: [
+        "https://github\.com/.*/commits/main",
+        "https://github\.com/.*/blob/main/musou_kokaton\.py"
+    ],
     5: [
         "https://github\.com/.*/blob/.*/README\.md",
         "https://github\.com/.*/blob/.*/.*\.py",
         "https://github\.com/.*/compare/main\.\.\..*"
     ],
     6: [
-        ".*stackoverflow\\.com.*",
-        ".*reddit\\.com.*",
-        ".*medium\\.com.*"
+        "https://github\.com/.*/blob/main/README\.md",
+        "https://github\.com/.*/blob/main/.*\.py"
     ],
+    7: [
+        "Nothing"
+    ],
+
     // 例外
     "default": [
         ".*bug\\exception.*"
