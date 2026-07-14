794c595a9dd2b5de4975e8a99b90d1b257fbae91
Njma <g212602618@edu.teu.ac.jp>
2026-07-14 18:50:34 +0900

    正規表現のリストを別ファイルに分離（EX5以外未設定）

---
+++ regex_config.js
@@ -0,0 +1,12 @@
+// 提出物のURLのリストを7回分用意して、切り替えする用
+const REGEX_LIST_EX5 = [
+    "https://github\.com/.*/blob/.*/README\.md",
+    "https://github\.com/.*/blob/.*/.*\.py",
+    "https://github\.com/.*/compare/main\.\.\..*"
+];
+
+const REGEX_LIST_EX2 = [
+    ".*stackoverflow\\.com.*",
+    ".*reddit\\.com.*",
+    ".*medium\\.com.*"
+];
