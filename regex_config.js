6daa7ca3055c3e40a2cc20a1275c8ee77292b9e9
Njma <g212602618@edu.teu.ac.jp>
2026-07-14 19:10:11 +0900

    URLの末尾に?ex=5のように指定することで結合するPDFの正規表現のリストをその回のものに変更できるように修正

--- regex_config.js
+++ regex_config.js
@@ -1,12 +1,17 @@
 // 提出物のURLのリストを7回分用意して、切り替えする用
-const REGEX_LIST_EX5 = [
-    "https://github\.com/.*/blob/.*/README\.md",
-    "https://github\.com/.*/blob/.*/.*\.py",
-    "https://github\.com/.*/compare/main\.\.\..*"
-];
-
-const REGEX_LIST_EX2 = [
-    ".*stackoverflow\\.com.*",
-    ".*reddit\\.com.*",
-    ".*medium\\.com.*"
-];
+const REGEX_LISTS = {
+    5: [
+        "https://github\.com/.*/blob/.*/README\.md",
+        "https://github\.com/.*/blob/.*/.*\.py",
+        "https://github\.com/.*/compare/main\.\.\..*"
+    ],
+    6: [
+        ".*stackoverflow\\.com.*",
+        ".*reddit\\.com.*",
+        ".*medium\\.com.*"
+    ],
+    // 例外
+    "default": [
+        ".*bug\\exception.*"
+    ]
+};
